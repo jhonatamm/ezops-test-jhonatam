d74bb4143260ca23e4a2f5266e467947f0de4d73
Jhonatam Medina <jhonatam.forever@gmail.com>
2020-01-31 22:51:12 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+var http = require('http').Server(app);
+
+//var io = require('socket.io').listen(server);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(__dirname));
+var server = app.listen(3000, () => {
+ console.log('server is running on port', server.address().port);
+});
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+});
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+  	io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+});
+
+
+
+
+mongoose.connect('mongodb+srv://medina:cT2yAZvJ6ancCB1h@medinacluster-t5evt.mongodb.net/simple-chat?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() =>{
+	console.log('conection to database established');
+}).catch(error => handleError(error));
+var Message = mongoose.model('Message',{ name : String, message : String});
+
+
+
+
+
+
+
+
+
+
